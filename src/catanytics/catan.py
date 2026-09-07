ecd115d406d641d04c3abf20c354e0eee022c53a
Mulmpages <mulmpages@gmail.com>
2026-09-07 22:14:13 +0200

    added ruff linter

--- src/catanytics/catan.py
+++ src/catanytics/catan.py
@@ -1,9 +1,9 @@
-from ressources import Ressources
+from catanytics.ressources import Ressources
 
 
 class Catan:
     def __init__(self):
-        # Turn -1  :    Player Selection 
+        # Turn -1  :    Player Selection
         # Turn  0  :    No dice throw; Initial Settlement Placement
         # Turn  1+ :    Dice Throw; Ressource Distribution
         self.turn = -1
@@ -11,29 +11,29 @@ class Catan:
         self.players = []
         self.winner = None
         # self.history = []
-        
+
         # Dice Throws are stored as ___[turn] -> int
         # Settlements, Robbers and Production are stored as ___[turn][player][ressource] -> int
         self.dice = []
         self.settlements = []
         self.robber = []
         self.production = []
-    
+
     # Access Game Information
     def is_player_selection(self):
-        return self.turn is -1
+        return self.turn == -1
 
     def is_initial_placement(self):
-        return self.turn is 0
+        return self.turn == 0
 
     def is_finished(self):
         return self.winner is not None
 
     def is_active(self):
         return (
-            (not self.is_player_selection()) and
-            (not self.is_initial_placement()) and
-            (not self.is_finished())
+            (not self.is_player_selection())
+            and (not self.is_initial_placement())
+            and (not self.is_finished())
         )
 
     def get_dice(self, turn):
@@ -54,7 +54,8 @@ class Catan:
 
     def add_settlement(self, player, ressources):
         for r in ressources:
-            self.settlements[self.turn][player][r] += ressources[r] # Can maybe remove the [r]?
+            self.settlements[self.turn][player][r] += ressources[r]
+            # Can maybe remove the [r]?
         self.calc_production()
 
     def set_robber(self, robber):
@@ -65,10 +66,9 @@ class Catan:
         production = self.empty_turn()
         for p in self.players:
             for r in Ressources:
-                production[p][r] = (
-                    self.get_settlements(self.turn, p, r)
-                    - self.get_robber(self.turn, p, r)
-                )
+                s = self.get_settlements(self.turn, p, r)
+                r = self.get_robber(self.turn, p, r)
+                production[p][r] = s - r
         self.production[self.turn] = production
 
     # Turn -1: Player Selection
