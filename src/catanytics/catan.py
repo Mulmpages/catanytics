7223401f91545423302aac7a1a7438b52a4bc7de
Mulmpages <mulmpages@gmail.com>
2026-09-09 21:22:00 +0200

    gitignore; new interface for catan.py

--- src/catanytics/catan.py
+++ src/catanytics/catan.py
@@ -1,106 +1,110 @@
-from catanytics.ressources import Ressources
+from catanytics.data import Data_Player, Resource
 
 
 class Catan:
-    def __init__(self):
+    def __init__(self, path : str | None = None):
         # Turn -1  :    Player Selection
         # Turn  0  :    No dice throw; Initial Settlement Placement
-        # Turn  1+ :    Dice Throw; Ressource Distribution
-        self.turn = -1
+        # Turn  1+ :    Dice Throw; Resource Distribution
+        self.turn : int = -1
 
-        self.players = []
-        self.winner = None
+        self.players : list[str] = []
+        self.winner : str = None
         # self.history = []
 
-        # Dice Throws are stored as ___[turn] -> int
-        # Settlements, Robbers and Production are stored as ___[turn][player][ressource] -> int
-        self.dice = []
-        self.settlements = []
-        self.robber = []
-        self.production = []
+        self.dice        : list[int]                    = []
+        self.settlements : list[dict[str, Data_Player]] = []
+        self.robber      : list[dict[str, Data_Player]] = []
+
+        # DANGER: Only temporary use, vulnerable to ACE
+        if path is not None:
+            with open(path, "r") as file:
+                lines = file.readlines()
+                self.turn        = eval(lines[0])
+                self.players     = eval(lines[1])
+                self.winner      = eval(lines[2])
+                self.dice        = eval(lines[3])
+                self.settlements = eval(lines[4])
+                self.robber      = eval(lines[5])
+
+    def __repr__(self) -> str:
+        return f"{self.turn}\n{self.players}\n{self.winner}\n{self.dice}\n{self.settlements}\n{self.robber}"
 
     # Access Game Information
-    def is_player_selection(self):
+    def is_player_selection(self) -> bool:
         return self.turn == -1
 
-    def is_initial_placement(self):
+    def is_initial_placement(self) -> bool:
         return self.turn == 0
 
-    def is_finished(self):
+    def is_finished(self) -> bool:
         return self.winner is not None
 
-    def is_active(self):
+    def is_active(self) -> bool:
         return (
             (not self.is_player_selection())
             and (not self.is_initial_placement())
             and (not self.is_finished())
         )
 
-    def get_dice(self, turn):
+    def player(self) -> str:
+        return self.players[(self.turn - 1) % len(self.players)]
+
+    # Access Game Information for Analysis
+    def get_dice(self, turn : int) -> int:
         return self.dice[turn]
 
-    def get_settlements(self, turn, player, ressource):
-        return self.settlements[turn][player][ressource]
+    def get_settlements(self, player : str, turn : int, dice : int, resource : Resource) -> int:
+        return self.settlements[turn][player][dice][resource]
 
-    def get_robber(self, turn, player, ressource):
-        return self.robber[turn][player][ressource]
+    def get_robber(self, player : str, turn : int, dice : int, resource : Resource) -> int:
+        return self.robber[turn][player][dice][resource]
 
-    def get_production(self, turn, player, ressource):
-        return self.production[turn][player][ressource]
+    def get_production(self, player : str, turn : int, dice : int, resource : Resource) -> int:
+        return self.production[turn][player][dice][resource]
 
     # Change Game Information
-    def set_dice(self, dice):
+    def set_dice(self, dice : int) -> None:
         self.dice[self.turn] = dice
 
-    def add_settlement(self, player, ressources):
-        for r in ressources:
-            self.settlements[self.turn][player][r] += ressources[r]
-            # Can maybe remove the [r]?
-        self.calc_production()
+    def add_settlement(self, player : str, resources : Data_Player) -> None:
+        for dice in resources:
+            for resource in resources[dice]:
+                self.settlements[self.turn][player][dice][resource] += resources[dice][resource]
 
-    def set_robber(self, robber):
+    def set_robber(self, robber : dict[str, Data_Player]) -> None:
         self.robber[self.turn] = robber
-        self.calc_production()
-
-    def calc_production(self):
-        production = self.empty_turn()
-        for p in self.players:
-            for r in Ressources:
-                s = self.get_settlements(self.turn, p, r)
-                r = self.get_robber(self.turn, p, r)
-                production[p][r] = s - r
-        self.production[self.turn] = production
 
     # Turn -1: Player Selection
-    def add_player(self, player):
+    def add_player(self, player : str) -> None:
         self.players.append(player)
 
-    def remove_player(self, player):
+    def remove_player(self, player : str) -> None:
         del self.players[player]
 
-    # Turn 0: No dice throw; Initial Settlement Placement
-    def start(self):
+    # Transition to Turn 0: No Dice Throw; Initial Settlement Placement
+    def start(self) -> None:
         self.dice.append(None)
-        self.settlements.append(self.empty_turn())
-        self.robber.append(self.empty_turn())
-        self.production.append(self.empty_turn())
-        self.started = True
+        data_player_empty = {p: Data_Player() for p in self.players}
+        self.settlements.append(data_player_empty)
+        self.robber.append(data_player_empty)
+        self.turn = 0
 
-    # Turn 1+: Dice Throw; Ressource Distribution
-    def next_turn(self):
+    # Transition to Turn 1+: Dice Throw; Resource Distribution
+    def next_turn(self) -> None:
         self.dice.append(None)
         self.settlements.append(self.settlements[self.turn])
         self.robber.append(self.robber[self.turn])
-        self.production.append(self.production[self.turn])
         self.turn += 1
 
-    # Finished: Winner is known
-    def finish(self, winner):
+    # Transition to Finished: Winner is known, Game ends
+    def finish(self, winner: str) -> None:
         self.winner = winner
 
     # Utility
-    def empty_turn(self):
-        return {p: Ressources.empty() for p in self.players}
+    def save(self, path : str) -> None:
+        with open(path, "w") as file:
+            file.write(repr(self))
 
-    def undo(self):
+    def undo(self) -> None:
         pass
---
+++ src/catanytics/data.py
@@ -0,0 +1,71 @@
+from enum import Enum, auto
+
+class Resource(Enum):
+    WOOL =   auto()
+    GRAIN =  auto()
+    LUMBER = auto()
+    BRICK =  auto()
+    ORE =    auto()
+
+class Data_Resource(dict):
+    KEYS = Resource
+
+    def __init__(self):
+        super().__init__()
+        for key in Data_Resource.KEYS:
+            self[key] = 0
+
+    def __eq__(self, other):
+        for key in Data_Resource.KEYS:
+            if self[key] != other[key]:
+                return False
+        return True
+
+    def __int__(self):
+        return sum(self.values())
+
+    def __add__(self, other):
+        result = Data_Resource()
+        for key in Data_Resource.KEYS:
+            result[key] = self[key] + other[key]
+        return result
+    
+    __radd__ = __add__
+
+    def __sub__(self, other):
+        result = Data_Resource()
+        for key in Data_Resource.KEYS:
+            result[key] = self[key] - other[key]
+        return result
+
+
+class Data_Player(dict):
+    KEYS = range(2, 13)
+
+    def __init__(self):
+        super().__init__()
+        for key in Data_Player.KEYS:
+            self[key] = Data_Resource()
+
+    def __eq__(self, other):
+        for key in Data_Player.KEYS:
+            if self[key] != other[key]:
+                return False
+        return True
+
+    def __abs__(self):
+        return sum(int(self.values()))
+
+    def __add__(self, other):
+        result = Data_Player()
+        for key in Data_Player.KEYS:
+            result[key] = self[key] + other[key]
+        return result
+    
+    __radd__ = __add__
+
+    def __sub__(self, other):
+        result = Data_Player()
+        for key in Data_Player.KEYS:
+            result[key] = self[key] - other[key]
+        return result
