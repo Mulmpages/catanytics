934fc4746a1a65f29b27ecf0ace7525ed718598b
Mulmpages <mulmpages@gmail.com>
2026-09-10 22:19:44 +0200

    tui CommandInput and layout

--- src/catanytics/data.py
+++ src/catanytics/data.py
@@ -2,11 +2,23 @@ from enum import Enum, auto
 
 
 class Resource(Enum):
-    WOOL =   auto()
-    GRAIN =  auto()
+    WOOL   = auto()
+    GRAIN  = auto()
     LUMBER = auto()
-    BRICK =  auto()
-    ORE =    auto()
+    BRICK  = auto()
+    ORE    = auto()
+
+    @staticmethod
+    def from_token(token : str) -> Resource:
+        # Example tokens: 'w', 'W'
+        match token.upper():
+            case "W": return Resource.WOOL
+            case "G": return Resource.GRAIN
+            case "L": return Resource.LUMBER
+            case "B": return Resource.BRICK
+            case "O": return Resource.ORE
+            case _: return None
+
 
 class Data_Resource(dict):
     KEYS = Resource
@@ -39,6 +51,16 @@ class Data_Resource(dict):
             result[key] = self[key] - other[key]
         return result
 
+    @staticmethod
+    def from_token(token : str) -> Data_Resource:
+        # Example tokens: 'w', 'W'
+        resource = Resource.from_token(token)
+        if resource is None:
+            return None
+        data_resource = Data_Resource()
+        data_resource[resource] = 1
+        return data_resource
+
 
 class Data_Player(dict):
     KEYS = range(2, 13)
@@ -70,3 +92,16 @@ class Data_Player(dict):
         for key in Data_Player.KEYS:
             result[key] = self[key] - other[key]
         return result
+
+    @staticmethod
+    def from_token(token : str) -> Data_Resource:
+        # Example tokens: 'w3', 'W12'
+        resource = Resource.from_token(token[0])
+        if resource is None:
+            return None
+        dice = int(token[1:])
+        if (dice not in Data_Resource.KEYS):
+            return None
+        data_player = Data_Player()
+        data_player[dice][resource] = 1
+        return data_player
--- src/catanytics/tui.py
+++ src/catanytics/tui.py
@@ -2,12 +2,11 @@ from textual.app import App, ComposeResult
 from textual.containers import Container, Vertical
 from textual.widgets import Footer, Header, Input, Label, RichLog
 
-from catanytics.catan import Catan, Data_Player, Resource
-
+from catanytics.catan import Catan, Data_Player
 
 class CommandInput(Input):
-    def __init__(self, catan : Catan):
-        super().__init__()
+    def __init__(self, catan : Catan, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.catan = catan
 
     def key_enter(self) -> None:
@@ -21,29 +20,24 @@ class CommandInput(Input):
             case "p-": return self.catan.remove_player(tokens[1])
             case "start": return self.catan.start()
             case "finish": return self.catan.finish()
-            case "s":
+            case "s": 
                 player = tokens[1]
-
-                return self.catan.add_settlement(*args)
-            case "r": return self.catan.add_settlement(*args)
-            else: 
-
-    @staticmethod
-    def token_to_resource(token : str) -> Resource:
-        match token[0]:
-            case "W": return Resource()WOOL
-            case "G": return Resource()GRAIN
-            case "L": return Resource()LUMBER
-            case "B": return Resource()BRICK
-            case "O": return Resource()ORE
-        dice = token[1]
+                data_player = sum([Data_Player.from_token(t) for t in tokens[2:]])
+                return self.catan.add_settlement(player, data_player)
+            case "r":
+                player = tokens[1]
+                data_player = sum([Data_Player.from_token(t) for t in tokens[2:]])
+                return self.catan.set_robber(data_player)
+            case "r0": return self.catan.remove_robber()
+            case _: pass
+        self.value = ""
 
 
 class TUI(App):
     CSS_PATH = "tui.tcss"
     TITLE = "Catanytics TUI"
     BINDINGS = [
-        ("q", "quit", "Quit"),
+        ("Ctrl+q", "quit", "Quit"),
         ("Ctrl+z", "undo", "Undo"),
         ("Ctrl+s", "save", "Save"),
         ("Ctrl+l", "load", "Load")
@@ -54,30 +48,30 @@ class TUI(App):
         self.catan = catan
 
     def action_save(self) -> None:
-        pass
+        self.history.write("action_save() is not implemented!")
 
     def action_load(self) -> None:
-        pass
+        self.history.write("action_load() is not implemented!")
 
     def action_undo(self) -> None:
-        pass
-
-
-
-    
+        self.history.write("action_undo() is not implemented!")
 
     def compose(self) -> ComposeResult:
+        self.analysis = Label(id="analysis")
+        self.hints = Label(id="hints")
+        self.history = RichLog(id="history")
+        self.history.can_focus = False
+        self.input = CommandInput(self.catan, id="input")
+
         yield Header(id="header")
         yield Footer(id="footer")
         with Container(id="content"):
             with Vertical(id="sidebar"):
-                yield Label(id="analysis")
-                yield Label(id="hints")
+                yield self.analysis
+                yield self.hints
             with Vertical():
-                myRichLog = RichLog(id="history")
-                myRichLog.can_focus = False
-                yield myRichLog
-                yield Input(id="input")
+                yield self.history
+                yield self.input
 
 
 if __name__ == "__main__":
