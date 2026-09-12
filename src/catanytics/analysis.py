d53a216501a22c2eff031493f90db7a648b3a916
MatthiasMG <matthias.gruenig@gmail.com>
2026-09-12 21:03:06 +0200

    update analysis

--- src/catanytics/analysis.py
+++ src/catanytics/analysis.py
@@ -5,13 +5,14 @@ class Analysis:
     def __init__(self, catan: Catan):
         self.catan = catan
 
-    def settlements_C(self, player, dice, resource):
+    def settlements_C(self, player, resource):
         turn = self.catan.turn
-        expProd = 0
+        production_total = 0
         for x in range(1, turn + 1):
-            expProd += self.catan.get_settlements(player, x, dice, resource)
+            dice = self.catan.get_dice(x)
+            production_total += self.catan.get_settlements(player, x, dice, resource)
 
-        return expProd
+        return production_total
 
     def robber_C(self, player, dice, resource):
         turn = self.catan.turn
